13fcae36d1bbe570975fdde7cac26df9ced0700a
Joulios74 <jules.segers@etu.univ-smb.fr>
2021-05-27 18:45:06 +0200

    monstre va plus dans les murs

---
+++ Assets/Scripts/Pont.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pont : MonoBehaviour
+{
+    [SerializeField]
+    private int nbItem;
+
+    [SerializeField]
+    private GameObject pontConstruit;
+    [SerializeField]
+    private GameObject pontMoitie;
+    [SerializeField]
+    private GameObject pontDetruit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            Debug.Log("startos");
+        pontDetruit.SetActive(true);
+        pontMoitie.SetActive(false);
+        pontConstruit.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nbItem >= 3)
+        {
+            Debug.Log("troisos");
+            pontDetruit.SetActive(false);
+            pontMoitie.SetActive(true);
+            pontConstruit.SetActive(false);
+        }
+        else if (nbItem > 4)
+        {
+            Debug.Log("cinquos");
+            pontDetruit.SetActive(false);
+            pontMoitie.SetActive(false);
+            pontConstruit.SetActive(true);
+        }
+    }
+}
